74230c39a6c245a1bba6acf14651d5447bab1c66
MinniMause <zucker_frei@yahoo.de>
2017-07-22 23:13:51 +0200

    add  Hallo-method .

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,12 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello Std!    ");
         System.out.println("Hello July!    ");
+        System.out.println(getHallo());
     }
+
+        private static String getHallo(){
+            return "Hallo aus getHallo()";
+        }
+
 }
+
